9f36cc0da135b92dbe8b58dabf72f07f28fa00d2
keanu-a <keanucaloua@gmail.com>
2026-06-12 00:57:06 -0700

    backend project setup

---
+++ backend/src/main/java/org/alouastudios/videoplatform/VideoPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.alouastudios.videoplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoPlatformApplication.class, args);
+    }
+
+}
