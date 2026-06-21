2f4f57ecb4689b732681d169c710b69dd3e8cf4a
keanu-a <keanucaloua@gmail.com>
2026-06-21 16:29:02 -0700

    feat: add sign out action

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from '@/components/ui/button';
+
 import { createClient } from '@/lib/supabase/server';
+import { signOut } from '@/actions/actions';
 
 export default async function Home() {
   const supabase = await createClient();
@@ -10,7 +13,18 @@ export default async function Home() {
     <div className="flex flex-col min-h-screen items-center justify-center">
       <h1 className="text-2xl font-semibold">Kyyros</h1>
       <div className="text-lg">
-        {user ? <p>Welcome, {user.email}!</p> : <p>Not logged in</p>}
+        {user ? (
+          <div>
+            <p>Welcome, {user.email}!</p>
+            <form action={signOut}>
+              <Button variant="outline" type="submit">
+                Sign Out
+              </Button>
+            </form>
+          </div>
+        ) : (
+          <p>Not logged in</p>
+        )}
       </div>
     </div>
   );
