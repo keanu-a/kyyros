660c34b94183825aeb3921f354f807d1c058d7f6
keanu-a <keanucaloua@gmail.com>
2026-06-23 12:53:51 -0700

    feat(backend): add User to Video and create VideoSummaryResponse DTO

--- backend/src/main/java/com/kyyros/model/Video.java
+++ backend/src/main/java/com/kyyros/model/Video.java
@@ -23,8 +23,9 @@ public class Video {
 
     // TODO: Add slug field for prettier URLs at a later time
 
-    @Column(name = "user_id", nullable = false)
-    private UUID userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User uploader;
 
     @Column(nullable = false)
     private String title;
